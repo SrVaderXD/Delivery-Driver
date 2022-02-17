7d6220e7c43792714a107dd75ad767fa0a5b5c1e
Henrique Loureiro de Faria <henriquelfaria08@gmail.com>
2022-02-17 16:20:34 -0300

    Changing car's color using GetComponent

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour // Deals with Collision
 {
-    
+
+    [SerializeField] Color32 hasPackageColor = new Color32 (1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32 (0,0,0,1);
+
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
-        void OnCollisionEnter2D(Collision2D other)
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Colliding!!");
     }
@@ -19,12 +30,14 @@ public class Delivery : MonoBehaviour // Deals with Collision
             Debug.Log("Package picked up!!");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
         }
 
         else if(other.tag == "Costumer" && hasPackage)
         {
             Debug.Log("Package delivered!!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
